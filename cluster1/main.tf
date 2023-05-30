d83e9295ba8ae9afcf702b777b970d02c1e68944
eilerb1011 <133176828+eilerb1011@users.noreply.github.com>
2023-05-30 14:07:08 -0400

    Update main.tf

--- cluster1/main.tf
+++ cluster1/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "kubernetes" {
-  config_path = "$workspace/cluster1/kubeconfig1.yaml"
+  config_path = "var.workspace/cluster1/kubeconfig1.yaml"
 }
 
 resource "kubernetes_deployment" "cluster1" {
