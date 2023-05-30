d1ae89f57efa47854640b01893c1d7286e2ff28b
eilerb1011 <133176828+eilerb1011@users.noreply.github.com>
2023-05-30 12:09:21 -0400

    Create main.tf

---
+++ cluster1/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+
+provider "kubernetes" {
+  config_path = "${WORKSPACE}/kubeconfig1"
+}
+
+resource "kubernetes_deployment" "cluster1" {
+  metadata {
+    name = "nginx-east"
+    labels = {
+      test = "east"
+      app  = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "cluster1" {
+  metadata {
+    name = "nginx-east"
+    labels = {
+      test = "east"
+      app  = "nginx"
+    }
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.CBC.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "LoadBalancer"
+    port {
+      port = 80
+    }
+  }
+}
