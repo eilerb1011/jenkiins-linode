fbf9e639ed02ff8b245156d3a19d7a98ba47e63a
eilerb1011 <133176828+eilerb1011@users.noreply.github.com>
2023-05-30 12:08:25 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "token" {
+  description = "set in project vars"
+  sensitive   = true
+}
+variable "k8s_version" {}
+variable "label" {}
+variable "region" {}
+variable "pools" {}
+variable "HA" {}
+variable "label2" {}
+variable "region2" {}
+variable "pools2" {}
+variable "HA2" {}
